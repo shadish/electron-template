4f4d65720b49fdbefed5a198b7af685eaf738550
shadish <shadtisch@gmail.com>
2018-07-05 23:23:37 -0600

    add basic app events

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron')
+  
+function create() {
+  browserWindow = new BrowserWindow({width: 800, height: 600})
+  browserWindow.loadFile('index.html')
+  browserWindow.webContents.openDevTools()
+  browserWindow.on('closed', () => {
+    browserWindow = null
+  }
+}
+  
+app.on('ready', create)
+
+app.on('window-all-closed', () => {
+	if(process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if(browserWindow === null) {
+		create()
+	}
+}
